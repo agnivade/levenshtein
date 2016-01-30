7a5028fe400e95367b971b53dae0bf4bf5d9f3de
Dave Chapeskie <github@ddm.wox.org>
2016-01-30 16:09:15 -0500

    Simplify tests and improve test output.
    * Splitting test cases with the exact same testing code
      between two tests provides no benifits so merge them.
    
    * On error, don't report or look at the score. Since the function
      never errors the error return should possibly be removed.
    
    * When a test fails report the function call made.

--- levenshtein_test.go
+++ levenshtein_test.go
@@ -2,47 +2,30 @@ package levenshtein
 
 import "testing"
 
-type testtuple struct {
-	first            string
-	second           string
-	levenshteinValue int
-}
-
-var tests = []testtuple{
-	{"", "hello", 5},
-	{"hello", "", 5},
-	{"hello", "hello", 0},
-}
-
 func TestSanity(t *testing.T) {
-	for i, test := range tests {
-		n, err := ComputeDistance(test.first, test.second)
-		if err != nil {
-			t.Errorf("Test[%d]: Error returned - %s. Value is - %d", i, err, n)
-		}
-		if n != test.levenshteinValue {
-			t.Errorf("Test[%d]: Expected %d, got %d", i, test.levenshteinValue, n)
-		}
+	tests := []struct {
+		a, b string
+		want int
+	}{
+		{"", "hello", 5},
+		{"hello", "", 5},
+		{"hello", "hello", 0},
+		{"ab", "aa", 1},
+		{"ab", "aaa", 2},
+		{"bbb", "a", 3},
+		{"kitten", "sitting", 3},
+		{"distance", "difference", 5},
+		{"levenshtein", "frankenstein", 6},
 	}
-}
-
-var tests1 = []testtuple{
-	{"ab", "aa", 1},
-	{"ab", "aaa", 2},
-	{"bbb", "a", 3},
-	{"kitten", "sitting", 3},
-	{"distance", "difference", 5},
-	{"levenshtein", "frankenstein", 6},
-}
-
-func TestNormal(t *testing.T) {
-	for i, test := range tests1 {
-		n, err := ComputeDistance(test.first, test.second)
+	for i, d := range tests {
+		n, err := ComputeDistance(d.a, d.b)
 		if err != nil {
-			t.Errorf("Test[%d]: Error returned - %s. Value is - %d", i, err, n)
+			t.Errorf("Test[%d]: ComputeDistance(%q,%q), error: %v", i, d.a, d.b, err)
+			continue
 		}
-		if n != test.levenshteinValue {
-			t.Errorf("Test[%d]: Expected %d, got %d", i, test.levenshteinValue, n)
+		if n != d.want {
+			t.Errorf("Test[%d]: ComputeDistance(%q,%q) returned %v, want %v",
+				i, d.a, d.b, n, d.want)
 		}
 	}
 }
