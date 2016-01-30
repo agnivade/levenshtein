82b29b804b418108b305d5e2d540676dd95293ad
Dave Chapeskie <github@ddm.wox.org>
2016-01-30 16:07:08 -0500

    Run `go fmt`.

--- levenshtein.go
+++ levenshtein.go
@@ -4,44 +4,43 @@
 package levenshtein
 
 import (
-  //"log"
-  "math"
-  )
+	"math"
+)
 
 // ComputeDistance computes the levenshtein distance between the two
 // strings passed as an argument. The return value is the levenshtein distance
 // and error if any
 func ComputeDistance(s1, s2 string) (n int, err error) {
 
-  if s1 == s2 {
-    return 0, nil
-  }
-  if len(s1) == 0 {
-    return len(s2), nil
-  }
-  if len(s2) == 0 {
-    return len(s1), nil
-  }
+	if s1 == s2 {
+		return 0, nil
+	}
+	if len(s1) == 0 {
+		return len(s2), nil
+	}
+	if len(s2) == 0 {
+		return len(s1), nil
+	}
 
-  x := make([]int, len(s2)+1)
-  y := make([]int, len(s2)+1)
-  for i, _ := range(x) {
-    x[i] = i
-  }
-  for i, _ := range(s1) {
-    y[0] = i+1
+	x := make([]int, len(s2)+1)
+	y := make([]int, len(s2)+1)
+	for i, _ := range x {
+		x[i] = i
+	}
+	for i, _ := range s1 {
+		y[0] = i + 1
 
-    for j, _ := range(s2) {
-      var cost int
-      if s1[i] == s2[j] {
-        cost = 0
-      } else {
-        cost = 1
-      }
-      y[j+1] = int(math.Min(float64(y[j]+1),
-                        math.Min(float64(x[j+1]+1), float64(x[j]+cost))))
-    }
-    copy(x, y)
-  }
-  return y[len(s2)], nil
+		for j, _ := range s2 {
+			var cost int
+			if s1[i] == s2[j] {
+				cost = 0
+			} else {
+				cost = 1
+			}
+			y[j+1] = int(math.Min(float64(y[j]+1),
+				math.Min(float64(x[j+1]+1), float64(x[j]+cost))))
+		}
+		copy(x, y)
+	}
+	return y[len(s2)], nil
 }
--- levenshtein_test.go
+++ levenshtein_test.go
@@ -3,45 +3,46 @@ package levenshtein
 import "testing"
 
 type testtuple struct {
-  first string
-  second string
-  levenshtein_value int
+	first             string
+	second            string
+	levenshtein_value int
 }
+
 var tests = []testtuple{
-  {"",  "hello",  5},
-  {"hello", "",  5},
-  {"hello",  "hello",  0},
+	{"", "hello", 5},
+	{"hello", "", 5},
+	{"hello", "hello", 0},
 }
 
 func TestSanity(t *testing.T) {
-  for i, test := range(tests) {
-    n, err := ComputeDistance(test.first, test.second)
-    if err != nil {
-      t.Errorf("Test[%d]: Error returned - %s. Value is - %d", i, err, n)
-    }
-    if n != test.levenshtein_value {
-      t.Errorf("Test[%d]: Expected %d, got %d", i, test.levenshtein_value, n)
-    }
-  }
+	for i, test := range tests {
+		n, err := ComputeDistance(test.first, test.second)
+		if err != nil {
+			t.Errorf("Test[%d]: Error returned - %s. Value is - %d", i, err, n)
+		}
+		if n != test.levenshtein_value {
+			t.Errorf("Test[%d]: Expected %d, got %d", i, test.levenshtein_value, n)
+		}
+	}
 }
 
 var tests1 = []testtuple{
-  {"ab",  "aa",  1},
-  {"ab", "aaa",  2},
-  {"bbb",  "a",  3},
-  {"kitten", "sitting", 3},
-  {"distance", "difference", 5},
-  {"levenshtein", "frankenstein", 6},
+	{"ab", "aa", 1},
+	{"ab", "aaa", 2},
+	{"bbb", "a", 3},
+	{"kitten", "sitting", 3},
+	{"distance", "difference", 5},
+	{"levenshtein", "frankenstein", 6},
 }
 
 func TestNormal(t *testing.T) {
-  for i, test := range(tests1) {
-    n, err := ComputeDistance(test.first, test.second)
-    if err != nil {
-      t.Errorf("Test[%d]: Error returned - %s. Value is - %d", i, err, n)
-    }
-    if n != test.levenshtein_value {
-      t.Errorf("Test[%d]: Expected %d, got %d", i, test.levenshtein_value, n)
-    }
-  }
+	for i, test := range tests1 {
+		n, err := ComputeDistance(test.first, test.second)
+		if err != nil {
+			t.Errorf("Test[%d]: Error returned - %s. Value is - %d", i, err, n)
+		}
+		if n != test.levenshtein_value {
+			t.Errorf("Test[%d]: Expected %d, got %d", i, test.levenshtein_value, n)
+		}
+	}
 }
