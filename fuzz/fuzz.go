7df6e981b249894ce5cbd944090d2049eade88c5
Agniva De Sarker <agnivade@yahoo.co.in>
2020-04-13 12:58:48 +0530

    Better fuzz testing
    We split the words on newline rather than half

--- fuzz/fuzz.go
+++ fuzz/fuzz.go
@@ -1,15 +1,26 @@
 package fuzz
 
-import "github.com/agnivade/levenshtein"
+import (
+	"strings"
+
+	"github.com/agnivade/levenshtein"
+)
 
 func Fuzz(data []byte) int {
 	str := string(data)
 	if len(str) == 0 {
 		return -1
 	}
-	splitIndex := len(str) / 2
-	s1 := str[:splitIndex]
-	s2 := str[splitIndex+1:]
-	levenshtein.ComputeDistance(s1, s2)
+	parts := strings.Split(str, "\n")
+	if len(parts) != 2 {
+		return -1
+	}
+	s1 := parts[0]
+	s2 := parts[1]
+	res := levenshtein.ComputeDistance(s1, s2)
+	// definitely an error.
+	if res < 0 || res > len(s1) || res > len(s2) {
+		return 0
+	}
 	return 1
 }
