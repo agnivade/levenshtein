b74372008936f73e045f6c90df7e49f9b880c6ea
psadac <paulsadac@gmail.com>
2020-05-08 11:45:52 +0200

    Improve performance by removing the "else" part of the matching condition
    Benchmark :
    name              old time/op    new time/op    delta
    Simple/ASCII-4       330ns ± 3%     294ns ± 4%  -10.97%  (p=0.000 n=19+19)
    Simple/French-4      609ns ± 4%     537ns ± 5%  -11.85%  (p=0.000 n=19+20)
    Simple/Nordic-4     1.26µs ± 5%    1.02µs ± 3%  -18.72%  (p=0.000 n=20+19)
    Simple/Tibetan-4    1.01µs ± 3%    0.89µs ± 3%  -12.71%  (p=0.000 n=18+19)
    [Geo mean]           712ns          615ns       -13.62%

--- levenshtein.go
+++ levenshtein.go
@@ -54,9 +54,8 @@ func ComputeDistance(a, b string) int {
 		prev := uint16(i)
 		var current uint16
 		for j := 1; j <= lenS1; j++ {
-			if s2[i-1] == s1[j-1] {
-				current = x[j-1] // match
-			} else {
+			current = x[j-1] // match
+			if s2[i-1] != s1[j-1] {
 				current = min(min(x[j-1]+1, prev+1), x[j]+1)
 			}
 			x[j-1] = prev
